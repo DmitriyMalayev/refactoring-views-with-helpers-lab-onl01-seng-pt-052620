3aca2ec1c7fc4017ee859f74d3b631e01d0f729c
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-10-22 02:46:32 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,33 @@
 module SongsHelper
-
 end
+
+
+# 1) songs /songs/:id links to the artist
+# Failure/Error: expect(page).to have_link("Daft Punk", href: artist_path(@artist))
+#   expected to find link "Daft Punk" with href "/artists/123" but there were no matches
+
+
+# ./spec/features/songs_spec.rb:16:in `block (3 levels) in <top (required)>'
+
+# [2] pry(#<#<Class:0x00007fd8ec3f8e38>>)> song.title
+# => "Bohemian Rhapsody"
+# [3] pry(#<#<Class:0x00007fd8ec3f8e38>>)> edit_artist_path(@song.id)
+# => "/artists/167/edit" 
+
+# Prefix Verb   URI Pattern                 Controller#Action
+# artists GET    /artists(.:format)          artists#index
+#         POST   /artists(.:format)          artists#create
+# new_artist GET    /artists/new(.:format)      artists#new
+# edit_artist GET    /artists/:id/edit(.:format) artists#edit
+#  artist GET    /artists/:id(.:format)      artists#show
+#         PATCH  /artists/:id(.:format)      artists#update
+#         PUT    /artists/:id(.:format)      artists#update
+#         DELETE /artists/:id(.:format)      artists#destroy
+#   songs GET    /songs(.:format)            songs#index
+#         POST   /songs(.:format)            songs#create
+# new_song GET    /songs/new(.:format)        songs#new
+# edit_song GET    /songs/:id/edit(.:format)   songs#edit
+#    song GET    /songs/:id(.:format)        songs#show
+#         PATCH  /songs/:id(.:format)        songs#update
+#         PUT    /songs/:id(.:format)        songs#update
+#         DELETE /songs/:id(.:format)        songs#destroy
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,31 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-  end
+   artist.name unless nil 
+   end
 
   def artist_name=(name)
+    @new_artist = Artist.find_or_create_by(name: name)
+    self.artist = @new_artist 
   end
 end
+
+
+# it "gets the artist name" do
+#   expect(@grid.artist_name).to eq("Daft Punk")
+# end
+
+# it "can set the artist via name" do
+#   song = Song.new(title: "Mad World")
+#   song.artist_name = "Tears for Fears"
+#   song.save
+#   expect(song.artist_name).to eq "Tears for Fears"
+#   expect(song.artist.name).to eq "Tears for Fears"
+# end
+
+# it "finds artist if already exists" do
+#   song = Song.create(title: "Around the World", artist_name: "Daft Punk")
+#   expect(song.artist_name).to eq "Daft Punk"
+#   expect(Artist.all.count).to eq 1
+# end
+# end
